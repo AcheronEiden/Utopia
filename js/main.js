2377fd458e534e832237aac5ea07701263de62f3
AcheronEiden <alhassan0000@yahoo.com>
2025-08-26 20:04:07 +0200

    added a centralized _footer page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Load the navigation bar
     fetch('/_nav.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('nav-placeholder').innerHTML = data;
+        });
+    // Load the footer
+    fetch('/_footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer-placeholder').innerHTML = data;
         });
 });
